5f409ca928ba1d6971de5f7859c3091f2985f05d
gatheluck <gatheluck+tech@gmail.com>
2021-11-04 21:38:43 +0900

    feat: add setting for input store.

--- applications/owan/owan/settings.py
+++ applications/owan/owan/settings.py
@@ -1,5 +1,6 @@
 import dataclasses
 import os
+import pathlib
 
 
 @dataclasses.dataclass(frozen=True)
@@ -8,9 +9,20 @@ class RedisSetting:
 
 
 @dataclasses.dataclass(frozen=True)
+class InputStoreSetting:
+    path: pathlib.Path
+
+
+@dataclasses.dataclass(frozen=True)
 class Settings:
     redis: RedisSetting
+    input_store: InputStoreSetting
 
 
 def settings() -> Settings:
-    return Settings(redis=RedisSetting(dns=os.getenv("REDIS_DSN", "redis://redis/0")))
+    return Settings(
+        redis=RedisSetting(dns=os.getenv("REDIS_DSN", "redis://redis/0")),
+        input_store=InputStoreSetting(
+            path=pathlib.Path(os.getenv("INPUT_STORE_DIR_PATH", "./tmp/input_store"))
+        ),
+    )
